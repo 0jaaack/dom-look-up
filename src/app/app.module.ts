16464fccb01526a2733433553dec2188cd5a210a
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-28 22:51:46 +0900

    [Setting] add search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,

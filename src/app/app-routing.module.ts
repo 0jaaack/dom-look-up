65f7b930e437c8b9bf2023e9b3b694837c0b4e5e
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-28 23:29:27 +0900

    [Setting] add view component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { SearchComponent } from "./search/search.component";
+import { ViewComponent } from "./view/view.component";
 
 const routes: Routes = [
   { path: "", component: SearchComponent },
+  { path: "/view", component: ViewComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SearchComponent } from "./search/search.component";
+import { ViewComponent } from "./view/view.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,

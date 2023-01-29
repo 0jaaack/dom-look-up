93bfdb7f6011d44a1a84fdca61f8228e3ed1272f
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-29 14:17:15 +0900

    [Setting] change css setting from css to scss

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-search",
   templateUrl: "./search.component.html",
-  styleUrls: ["./search.component.css"]
+  styleUrls: ["./search.component.scss"]
 })
 export class SearchComponent {
 
